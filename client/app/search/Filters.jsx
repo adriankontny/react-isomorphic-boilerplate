5526fe10480827033a3a4a52970262eb7db29578
Adrian Kontny <adrian.kontny@almservices.pl>
2019-02-14 02:01:28 -0600

    mui: swipes

---
+++ client/app/search/Filters.jsx
@@ -0,0 +1,139 @@
+import { withStyles } from '@material-ui/core/styles';
+import {
+  Typography,
+} from '@material-ui/core';
+import React from 'react';
+import { connect } from 'react-redux';
+import SwipeableViews from "react-swipeable-views";
+import Animated from "animated/lib/targets/react-dom";
+
+const slides = [
+  { backgroundColor: "#fff" },
+  { backgroundColor: "#fff" },
+  { backgroundColor: "#fff" },
+  { backgroundColor: "#fff" },
+  { backgroundColor: "#fff" },
+  { backgroundColor: "#fff" }
+];
+
+const styles = {
+  swipeableViews: {
+    padding: 80
+  },
+  slide: {
+    opacity: 0.3,
+    scale: 0.8,
+    padding: 25,
+    animatedDiv: {
+      minHeight: 400,
+      backgroundColor: "#bbb"
+    },
+  }
+};
+
+class Filters extends React.Component {
+
+  constructor(props){
+    super(props)
+    this.state = {
+      index: 0,
+      windowWidth: (window && window.innerWidth) || 1000,
+      position: new Animated.Value(0)
+    };
+    this.updateDimensions = this.updateDimensions.bind(this)
+    this.handleChangeIndex = this.handleChangeIndex.bind(this)
+    this.handleSwitch = this.handleSwitch.bind(this)
+  }
+
+  updateDimensions() {
+    this.setState({ windowWidth: window.innerWidth });
+  };
+  componentDidMount() {
+    this.updateDimensions();
+    window.addEventListener("resize", this.updateDimensions);
+  };
+  componentWillUnmount() {
+    window.removeEventListener("resize", this.updateDimensions);
+  };
+  handleChangeIndex(index) {
+    this.setState({ index });
+  };
+  handleSwitch(index, type) {
+    if (type === "end") {
+      Animated.spring(this.state.position, { toValue: index }).start();
+      return;
+    }
+    this.state.position.setValue(index);
+  };
+
+  render() {
+    const { index, position, windowWidth } = this.state;
+    return (
+      <SwipeableViews
+        enableMouseEvents
+        style={styles.swipeableViews}
+        onChangeIndex={this.handleChangeIndex}
+        onSwitching={this.handleSwitch}
+      >
+        {slides.map((slide, slideIndex) => {
+          const inputRange = slides.map((_, i) => i);
+          const scale = position.interpolate({
+            inputRange,
+            outputRange: inputRange.map(i => {
+              return slideIndex === i ? 1 : styles.slide.scale;
+            })
+          });
+          const opacity = position.interpolate({
+            inputRange,
+            outputRange: inputRange.map(i => {
+              return slideIndex === i ? 1 : styles.slide.opacity;
+            })
+          });
+          const translateX = position.interpolate({
+            inputRange,
+            outputRange: inputRange.map(i => {
+              const halfPadding = styles.slide.padding / 2;
+              const translate = windowWidth / (halfPadding * styles.slide.scale) - halfPadding;
+              if (i > slideIndex) {
+                return translate;
+              } else if (i < slideIndex) {
+                return -translate;
+              } else {
+                return 0;
+              }
+            })
+          });
+          return (
+            <div
+              style={{ padding: styles.slide.padding }}
+            >
+              <Animated.div
+                key={String(slideIndex)}
+                style={{
+                    opacity,
+                    transform: [{ scale }, { translateX }],
+                    ...styles.slide.animatedDiv,
+                  }}
+              >
+                {this.props.children}
+              </Animated.div>
+            </div>
+          );
+        })}
+      </SwipeableViews>
+    );
+  }
+}
+
+const mapStateToProps = state => ({
+  state,
+});
+
+const mapDispatchToProps = dispatch => ({
+  dispatch,
+});
+
+export default withStyles(styles, { withTheme: true })(connect(
+  mapStateToProps,
+  mapDispatchToProps,
+)(Filters));
--- client/app/search/Search.jsx
+++ client/app/search/Search.jsx
@@ -8,6 +8,8 @@ import { connect } from 'react-redux';
 import SideBarLeft from './SideBarLeft.jsx'
 import TopBar from './TopBar.jsx'
 import Content from './Content.jsx'
+import Filters from './Filters.jsx'
+import Filter from './Filter.jsx'
 
 const styles = () => ({
   root: {
@@ -25,7 +27,9 @@ class Search extends React.Component {
         <CssBaseline />
         <TopBar />
         <SideBarLeft />
-        <Content />
+        <Filters>
+          <Filter></Filter>
+        </Filters>
       </div>
     );
   }
