5e67ea28b412665543e6b5a6c8dd5b02b39f5d1b
johndoe <adrian.kontny@outlook.com>
2019-04-14 12:52:47 +0200

    mui: distinguishable filters that take actions

--- client/app/search/filters/Filter.jsx
+++ client/app/search/filters/Filter.jsx
@@ -3,21 +3,21 @@ import FilterRange from './filter/FilterRange'
 import FilterMultiselect from './filter/FilterMultiselect'
 
 const Filter = props => {
-  const { filter, upload } = props
+  const { filter, upload, filtersObjectPath } = props
   const { type } = filter;
 
   switch (type) {
     case "range":
       return (
         <FilterRange
-          {...{ filter, upload }}
+          {...{ filter, upload, filtersObjectPath }}
         ></FilterRange>
       )
 
     case "multiselect":
       return (
         <FilterMultiselect
-          {...{ filter, upload }}
+          {...{ filter, upload, filtersObjectPath }}
         ></FilterMultiselect>
       )
 
